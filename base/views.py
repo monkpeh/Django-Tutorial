eea077af9120ee65809d2f1436c3a161b32ae366
monkpeh <monkpehk35@gmail.com>
2025-02-22 11:01:54 -0500

    Chat Room Messages CRUD

--- base/views.py
+++ base/views.py
@@ -5,6 +5,7 @@ from django.contrib.auth.decorators import login_required
 from django.db.models import Q
 from django.contrib.auth.models import User
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.forms import UserCreationForm
 from .models import Room, Topic
 from .forms import RoomForm
 # Create your views here.
@@ -18,7 +19,7 @@ from .forms import RoomForm
 
 
 def loginPage(request):
-    
+    page = 'login'
     if request.user.is_authenticated:
         return redirect('home')
     
@@ -39,12 +40,28 @@ def loginPage(request):
         else:
             messages.error(request, 'Username OR password does not exist')
         
-    context = {}
+    context = {'page':page}
     return render(request, 'base/login_register.html', context)
 
 def logoutUser(request):
     logout(request)
     return redirect('home')
+
+def registerPage(request):
+    form = UserCreationForm()
+    
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save(commit=False)
+            user.username = user.username.lower()
+            user.save()
+            login(request, user) 
+            return redirect('home')
+        else:
+            messages.error(request, 'An error occurred during registration')
+               
+    return render(request, 'base/login_register.html',{'form' :form})
 
 def home(request):
     q = request.GET.get('q') if request.GET.get('q') != None else ''
